31ec55db391de3afa54a93d5b778f4d08bde1834
aboynejames <aboynejames@gmail.com>
2025-06-26 19:39:26 +0100

    update for absolute paths for windows

--- models-hop/index.js
+++ models-hop/index.js
@@ -1,4 +1,19 @@
-export { default as AverageModel } from './statistics/average.js';
-export { default as SumModel } from './statistics/sum.js';
-export { default as LinearRegression } from './statistics/linear-regression.js';
-export { default as AutoRegression } from './statistics/auto-regression.js';
+import { join } from 'path';
+import { homedir } from 'os';
+
+const avgPath = join(homedir(), '.hop-models', 'statistics', 'average.js');
+const sumPath = join(homedir(), '.hop-models', 'statistics', 'sum.js');
+const linregPath = join(homedir(), '.hop-models', 'statistics', 'linear-regression.js');
+const autoregPath = join(homedir(), '.hop-models', 'statistics', 'auto-regression.js');
+
+const { default: AverageModel } = await import(avgPath);
+const { default: SumModel } = await import(sumPath);
+const { default: LinearRegression } = await import(linregPath);
+const { default: AutoRegression } = await import(autoregPath);
+
+export {
+  AverageModel,
+  SumModel,
+  LinearRegression,
+  AutoRegression
+};
